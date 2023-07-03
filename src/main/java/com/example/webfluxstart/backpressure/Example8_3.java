82b0b7c988b18916182224ba38705c686d4d4ec6
JxxHxxx <leesin5498@naver.com>
2023-07-04 04:13:54 +0900

    Hot, Cold Sequence & backpressure 예제

---
+++ src/main/java/com/example/webfluxstart/backpressure/Example8_3.java
@@ -0,0 +1,25 @@
+package com.example.webfluxstart.backpressure;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+@Slf4j
+public class Example8_3 {
+    public static void main(String[] args) throws InterruptedException {
+        Flux.interval(Duration.ofMillis(1L))
+                .onBackpressureDrop(dropped -> log.info("# dropped: {}", dropped))
+                .publishOn(Schedulers.parallel())
+                .subscribe(data -> {
+                            try {
+                                Thread.sleep(5L);
+                            } catch (InterruptedException e) {
+                            }
+                            log.info("# onNext: {}", data);
+                        },
+                        error -> log.error("# onError", error));
+        Thread.sleep(2000L);
+    }
+}
