16f8c44ceddfdc106e11eb0fe2fb109d46555f80
JxxHxxx <leesin5498@naver.com>
2023-07-26 00:13:08 +0900

    Sequence 생성 Operator

---
+++ src/main/java/com/example/webfluxstart/operators/create/FromIterable.java
@@ -0,0 +1,37 @@
+package com.example.webfluxstart.operators.create;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@Slf4j
+public class FromIterable {
+
+    public static void main(String[] args) {
+        Flux
+            .fromIterable(People.samples())
+            .subscribe(people -> log.info("이름 {}, 나이 {}", people.getName(), people.getAge())
+            );
+    }
+
+    @Getter
+    static class People {
+        String name;
+        Integer age;
+
+        public People(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public static List<People> samples() {
+            return List.of(
+                    new People("jxx", 15),
+                    new People("xuni", 24),
+                    new People("yani", 21),
+                    new People("shy", 39));
+        }
+    }
+}
